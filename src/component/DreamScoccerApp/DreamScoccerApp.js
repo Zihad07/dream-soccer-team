b059de68bcd2182710959784066ae9e5664d626a
zihad07 <zihadnayem8gmail.com>
2021-07-10 20:48:57 +0600

    load players data and show.

--- src/component/DreamScoccerApp/DreamScoccerApp.js
+++ src/component/DreamScoccerApp/DreamScoccerApp.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import playersData from "../../PlayersData/playersJsonData.js";
 import Player from "../Player/Player";
 
 const DreamScoccerApp = () => {
+	const [players, setPlayers] = useState([]);
+	useEffect(() => {
+		setPlayers(playersData);
+	}, []);
+	console.log(players);
 	return (
 		<div className="container">
-			<div className="col-md-8">
-				<Player></Player>
+			<div className="row">
+				<div className="col-md-8 border-right border-info">
+					<div className="card-columns">
+						{players.map((pl) => (
+							<Player player={pl} key={pl.id}></Player>
+						))}
+					</div>
+				</div>
+				<div className="col-md-4"></div>
 			</div>
-			<div className="col-md-4"></div>
 		</div>
 	);
 };
--- src/component/Player/Player.js
+++ src/component/Player/Player.js
@@ -1,44 +1,35 @@
 import React from "react";
 import "./Player.css";
 
-const Player = () => {
+const Player = (props) => {
+	const { id, name, age, nationality, photo: image, salary } = props.player;
 	return (
 		<div className="card p-3">
 			<div className="d-flex align-items-center">
 				<div className="image">
-					<img
-						src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-						className="rounded"
-						width="155"
-					/>
+					<img src={image} className="rounded-circle" width="155" />
 				</div>
 				<div className="ml-3 w-100">
-					<h4 className="mb-0 mt-0">Alex Morrision</h4>{" "}
-					<span>Senior Journalist</span>
+					<h4 className="mb-0 mt-0">{name}</h4>{" "}
+					<span>Country: {nationality}</span>
 					<div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
 						<div className="d-flex flex-column">
 							{" "}
-							<span className="articles">Articles</span>{" "}
-							<span className="number1">38</span>{" "}
+							<span className="rating">Age</span>{" "}
+							<span className="number3">{age}</span>{" "}
 						</div>
 						<div className="d-flex flex-column">
 							{" "}
-							<span className="followers">Followers</span>{" "}
-							<span className="number2">980</span>{" "}
-						</div>
-						<div className="d-flex flex-column">
-							{" "}
-							<span className="rating">Rating</span>{" "}
-							<span className="number3">8.9</span>{" "}
+							<small className="text-success font-font-weight-bolder">
+								Salary
+							</small>{" "}
+							<span className="number3">${salary}</span>{" "}
 						</div>
 					</div>
 					<div className="button mt-2 d-flex flex-row align-items-center">
 						{" "}
-						<button className="btn btn-sm btn-outline-primary w-100">
-							Chat
-						</button>{" "}
 						<button className="btn btn-sm btn-primary w-100 ml-2">
-							Follow
+							Add To Your Team
 						</button>
 					</div>
 				</div>
